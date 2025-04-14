c8500610f0aa7bacf5cbc6a810ff9ef6fa4d2350
ssophei <sophie.nguyen@berkeley.edu>
2025-04-14 03:17:39 -0700

    basic ui

---
+++ script.js
@@ -0,0 +1,38 @@
+const characterrange = document.getElementById("characterrange")
+const characternumber = document.getElementById("characternumber")
+const say = document.getElementById("say")
+const uppercase = document.getElementById("uppercase")
+const lowercase = document.getElementById("lowercase")
+const numbers = document.getElementById("numbers")
+const symbols = document.getElementById("symbols")
+const form = document.getElementById("passwordgeneratorform")
+const passwordDisplay = document.getElementById("passwordDisplay")
+
+characterrange.addEventListener("input", synchCharacterAmount)
+characternumber.addEventListener("input", synchCharacterAmount)
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    const length = characternumber.value
+    const say = say.checked
+    const uppercase = uppercase.checked
+    const lowercase = lowercase.checked
+    const symbols = symbols.checked
+    const numbers = numbers.checked
+    const password = generatePassword(length, say, uppercase, lowercase, numbers, symbols)
+    passwordDisplay.innerText = "banana"
+})
+
+function generatePassword(length, say, readability, uppercase, lowercase, numbers, symbols){
+    function getRandomNumber(min, max){
+        return Math.floor(Math.random() * (max - min)) + min
+    }
+    const pass = getRandomNumber.toString()
+    return pass
+}
+
+function synchCharacterAmount(e) {
+    const value = e.target.value
+    characterrange.value = value
+    characternumber.value = value
+}
